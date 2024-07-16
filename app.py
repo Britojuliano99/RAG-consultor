097a325633132103b60cb993568c116e88dbc30b
Britojuliano99 <britojuliano99@gmail.com>
2024-07-16 14:02:37 -0300

    Segundo commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from rag_model import *
 
 prompt = st.chat_input("Fale alguma coisa")
-choice = st.radio('Qual modelo',['Llamma 3','ChatGPT4o'])
+choice = st.radio('Qual modelo',['Llamma 3-rag','Llamma 3-puro','ChatGPT4o'])
 
-if choice == 'Llamma 3':
+if choice == 'Llamma 3-rag':
         st.write('LLAMA 3')
         if prompt:
             st.write(f" {prompt}")
@@ -16,6 +16,20 @@ if choice == 'Llamma 3':
             resposta = chat_llama_rag(prompt)
 
             message.write(resposta)
+
+if choice == 'Llamma 3-puro':
+        st.write('LLAMA 3')
+        if prompt:
+            st.write(f" {prompt}")
+
+            message = st.chat_message("assistant")
+            message.write("Ola vou ajudar com sua pergunta")
+
+
+            resposta = chat_llama_sem_rag(prompt)
+
+            message.write(resposta)
+
 
 if choice == 'ChatGPT4o':
         st.write('ChatGPT4o')
--- rag_model.py
+++ rag_model.py
@@ -1,7 +1,6 @@
 from openai import OpenAI
 from typing import List, Optional
 from groq import Groq
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
 from chaves import *
 from pinecone.grpc import PineconeGRPC as Pinecone
@@ -12,7 +11,7 @@ client_OpenAI = OpenAI(api_key=OPENAI_API_KEY)
 
 pc = Pinecone(api_key=API_KEY_PINECONE)
 
-index_name="ragconsultor"
+index_name="rag-comprimento-sentenca"
 
 client_Groq = Groq(
     api_key=GROQ_API_KEY,
@@ -56,10 +55,30 @@ def chat_llama_rag(text):
         messages=[
             {
                 "role": "system",
+                'content':'Os textos abaixos são sentenças em segunda instancias sobre processos que podem ser relevantes para a pergunta do usuario.',
                 'content':textos_similares[0],
                 'content':textos_similares[1],
                 'content':textos_similares[2],
                 'content':'Dado esses contextos acima responda a pergunta?',
+            },
+            {
+                'role':'user',
+                'content':text
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    return chat_completion.choices[0].message.content
+
+def chat_llama_sem_rag(text):
+
+   
+    
+    chat_completion = client_Groq.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": 'Eu sou um assistente oque posso ajudar?'
             },
             {
                 'role':'user',
@@ -76,6 +95,7 @@ def chat_gpt_rag(text):
     
     # Preparando as mensagens para o chat
     messages = [
+        {"role": "system", "content": 'Os textos abaixos são sentenças em segunda instancias sobre processos que podem ser relevantes para a pergunta do usuario.'},
         {"role": "system", "content": textos_similares[0]},
         {"role": "system", "content": textos_similares[1]},
         {"role": "system", "content": textos_similares[2]},
